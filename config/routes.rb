af022a237db8a311ac106e74da29bae1b0159154
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-01-06 22:09:17 +0100

    add importmap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'main/index'
-  get 'about/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  get 'main/index', to: "main#index"
+  get 'about', to: "about#index"
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "main#index"
 end
