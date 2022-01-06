30aadf9c9fdc3c79b6331a8edd02adfd1cbcd33f
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-01-06 22:21:56 +0100

    add vue

---
+++ app/javascript/controllers/component_controller.js
@@ -0,0 +1,14 @@
+import * as Vue from "vue"
+const point = "#point"
+const element = document.querySelector(point)
+if (element !== null) {
+  const app = Vue.createApp({
+    data() {
+      return { count: 1 }
+    },
+    created() {
+      console.log("count is: "+ this.count) // => "count is: 1"
+    }
+  })
+  const vm = app.mount(point)
+}
